b19998a20f7086c6464819a7f96c9ebb4f340335
AgentDan <bion-1@mail.ru>
2025-02-24 17:10:27 +0100

    commit

--- client/src/state/state.jsx
+++ client/src/state/state.jsx
@@ -378,7 +378,7 @@ export const store = [
         projects: [
             {
                 id: v1(),
-                check: true,
+                check: false,
                 nameProject: "var1",
                 def: [
                     {file: "awingDefoult"},
